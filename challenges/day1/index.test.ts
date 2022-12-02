82d841f0bf7671323af0998df15a852b5022efa6
ccp92 <mail@chrisparsons.dev>
2022-12-02 12:16:51 +0000

    Add solution for Day1, Part1

--- challenges/day1/index.test.ts
+++ challenges/day1/index.test.ts
@@ -1,9 +1,35 @@
-import highestCalories from ".";
+import highestCalories, { convertFileToInput } from ".";
 
 describe("Day 1", () => {
-  it("should return a sum of numbers when only one elf", () => {
-    const input = [1, 2, 3, 4];
-    const result = highestCalories(input);
-    expect(result).toBe(10);
+  describe("Part 1", () => {
+    it("should return a sum of numbers when only one elf", () => {
+      const input = [[1, 2, 3, 4]];
+      const result = highestCalories(input);
+      expect(result).toBe(10);
+    });
+
+    it("should return the elf with the highest calories when 2 are provided", () => {
+      const input = [
+        [1, 2, 3],
+        [4, 5, 6],
+      ];
+      const result = highestCalories(input);
+      expect(result).toBe(15);
+    });
+
+    it("should convert the text from a file to an array", () => {
+      const input = convertFileToInput(__dirname + "/testInput.txt");
+      expect(input).toStrictEqual([
+        [5686, 2211, 1513, 7036, 5196, 10274, 2967, 2551],
+        [5942, 5827, 2514, 4024],
+        [9857, 13173, 13071, 17540],
+      ]);
+    });
+
+    it("should return the highest calories from the file", () => {
+      const input = convertFileToInput(__dirname + "/challengeInput.txt");
+      const result = highestCalories(input);
+      expect(result).toBe(66719);
+    });
   });
 });
--- challenges/day1/index.ts
+++ challenges/day1/index.ts
@@ -1,6 +1,29 @@
-const highestCalories = (foods: number[]) => {
-  const calorieSum = foods.reduce((sum, food) => sum + food, 0);
-  return calorieSum;
+import fs from "fs";
+
+export const convertFileToInput = (file: string) => {
+  const string = fs.readFileSync(file, "utf8").toString();
+  const array = string.split("\n");
+
+  const allElves: number[][] = [];
+  let individualElf: number[] = [];
+  array.forEach((element) => {
+    if (element.length > 0) {
+      individualElf.push(parseInt(element));
+    } else {
+      allElves.push(individualElf);
+      individualElf = [];
+    }
+  });
+
+  return allElves;
+};
+
+const highestCalories = (elves: number[][]) => {
+  let calorieSum: number[] = [];
+  elves.forEach((elf) => {
+    calorieSum.push(elf.reduce((a, b) => a + b, 0));
+  });
+  return Math.max(...calorieSum);
 };
 
 export default highestCalories;
