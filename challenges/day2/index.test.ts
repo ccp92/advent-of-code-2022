52847a8310b792fc08751072ca67349139341503
ccp92 <mail@chrisparsons.dev>
2022-12-02 12:55:42 +0000

    Add solution for Day2 Part2

--- challenges/day2/index.test.ts
+++ challenges/day2/index.test.ts
@@ -1,4 +1,4 @@
-import calculatedScore, { convertFileToInput } from ".";
+import calculatedScore, { convertFileToInput, updatedCalculatedScore } from ".";
 
 describe("Day 2", () => {
   describe("Part 1", () => {
@@ -11,20 +11,36 @@ describe("Day 2", () => {
       const result = calculatedScore(input);
       expect(result).toBe(15);
     });
-  });
+    it("Should return an array of arrays", () => {
+      const input = convertFileToInput(__dirname + "/testInput.txt");
+      expect(input).toStrictEqual([
+        ["A", "Y"],
+        ["B", "Y"],
+        ["B", "Z"],
+      ]);
+    });
 
-  it("Should return an array of arrays", () => {
-    const input = convertFileToInput(__dirname + "/testInput.txt");
-    expect(input).toStrictEqual([
-      ["A", "Y"],
-      ["B", "Y"],
-      ["B", "Z"],
-    ]);
+    it("should return the challenge score", () => {
+      const input = convertFileToInput(__dirname + "/challengeInput.txt");
+      const result = calculatedScore(input);
+      expect(result).toBe(13565);
+    });
   });
+  describe("Part 2", () => {
+    it("should return a score for rock, paper or scissors", () => {
+      const input = [
+        ["A", "Y"],
+        ["B", "X"],
+        ["C", "Z"],
+      ];
+      const result = updatedCalculatedScore(input);
+      expect(result).toBe(12);
+    });
 
-  it("should return the challenge score", () => {
-    const input = convertFileToInput(__dirname + "/challengeInput.txt");
-    const result = calculatedScore(input);
-    expect(result).toBe(13565);
+    it("should return the challenge score", () => {
+      const input = convertFileToInput(__dirname + "/challengeInput.txt");
+      const result = updatedCalculatedScore(input);
+      expect(result).toBe(12424);
+    });
   });
 });
