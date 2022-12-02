20d8b0505d23ef79bc4bdc16b62b2c8f1421ec89
ccp92 <mail@chrisparsons.dev>
2022-12-02 12:55:42 +0000

    Add solution for Day2 Part2

--- challenges/day2/index.ts
+++ challenges/day2/index.ts
@@ -66,4 +66,70 @@ const calculatedScore = (results: string[][]) => {
   return score;
 };
 
+export const updatedCalculatedScore = (results: string[][]) => {
+  let score = 0;
+
+  // Opponent throws
+  // A = rock
+  // B = paper
+  // C = scissors
+
+  // You throw
+  // rock = 1
+  // paper = 2
+  // scissors = 3
+
+  // Result is
+  // X = lose
+  // Y = draw
+  // Z = win
+
+  // Scores
+  // 6 = win
+  // 3 = draw
+  // 0 = lose
+
+  results.forEach((result) => {
+    if (result[0] === "A" && result[1] === "X") {
+      // They throw rock, you lose, you throw scissors
+      score += 0;
+      score += 3;
+    } else if (result[0] === "A" && result[1] === "Y") {
+      // They throw rock, you draw, you throw rock
+      score += 3;
+      score += 1;
+    } else if (result[0] === "A" && result[1] === "Z") {
+      // They throw rock, you win, you throw paper
+      score += 6;
+      score += 2;
+    } else if (result[0] === "B" && result[1] === "X") {
+      // They throw paper, you lose, you throw rock
+      score += 0;
+      score += 1;
+    } else if (result[0] === "B" && result[1] === "Y") {
+      // They throw paper, you draw, you throw paper
+      score += 3;
+      score += 2;
+    } else if (result[0] === "B" && result[1] === "Z") {
+      // They throw paper, you win, you throw scissors
+      score += 6;
+      score += 3;
+    } else if (result[0] === "C" && result[1] === "X") {
+      // They throw scissors, you lose, you throw paper
+      score += 0;
+      score += 2;
+    } else if (result[0] === "C" && result[1] === "Y") {
+      // They throw scissors, you draw, you throw scissors
+      score += 3;
+      score += 3;
+    } else if (result[0] === "C" && result[1] === "Z") {
+      // They throw scissors, you win, you throw rock
+      score += 6;
+      score += 1;
+    }
+  });
+
+  return score;
+};
+
 export default calculatedScore;
